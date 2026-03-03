c3c726009aed5de4fd5856251c44ead01ae21fb1
devdesai06 <devdesai8790@gmail.com>
2026-03-03 18:49:30 +0530

    Deployment fix

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 connectDB()
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.APP_URL || "http://localhost:5173",
     credentials: true,
   })
 );
